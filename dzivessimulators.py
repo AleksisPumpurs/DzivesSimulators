b032e5fb77801b402dca4bc77f1b44e52f809b1e
AleksisPumpurs <pucensaleksis@gmail.com>
2024-06-22 15:02:29 +0300

    Add files via upload

---
+++ dzivessimulators.py
@@ -0,0 +1,16 @@
+print("Tu pamodies sava gulta, tevi sauc aleksis, ko tu dari?")
+option1 = input("Pamizt, Kakat, Turpinat gulet,  uzdirst uz maksa.")
+beigas = "spele beidzas"
+
+if (option1 == "Turpinat gulet"):
+    print("tu uzbraksti uzn pamiz bikses")
+    print(beigas)
+elif (option1 == "Pamizt"):
+    print("Maksims atnaks un skatisies ka tu pamiz un tu vinam uzkaka virsu")
+elif (option1 =="Kakat"):
+    print("Tu apkakaji esi jo tu netiki uz toleti atri")
+elif (option1 =="Uzdirst maksism"):
+    print("Good")
+    
+    
+
